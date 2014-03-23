b413ba4a86bb38ed16c73e161ca1a5b896bec2d2
Aaahh Ahh <woohoomoo2u@gmail.com>
2014-03-23 02:20:03 -0400

    Changed to copying framaroot to app dir, more work on "install bootmenu".

--- app/src/main/java/com/Aaahh/myapplication2/app/MainActivity.java
+++ app/src/main/java/com/Aaahh/myapplication2/app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.Aaahh.myapplication2.app;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.net.Uri;
@@ -13,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -66,7 +69,7 @@ public class MainActivity extends ActionBarActivity {
         InputStream in;
         OutputStream out;
         in = assetManager.open("framaroot.apk");
-        out = new FileOutputStream("/sdcard/framaroot.apk");
+            out = new FileOutputStream(getExternalFilesDir(null) + "/framaroot.apk");
         byte[] buffer = new byte[1024];
         int read;
         while ((read = in.read(buffer)) != -1) {
@@ -76,7 +79,7 @@ public class MainActivity extends ActionBarActivity {
         out.flush();
         out.close();
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(new File("/sdcard/framaroot.apk")), "application/vnd.android.package-archive");
+            intent.setDataAndType(Uri.fromFile(new File(getExternalFilesDir(null) + "/framaroot.apk")), "application/vnd.android.package-archive");
         startActivityForResult(intent, 1);
     }
     }
@@ -86,6 +89,21 @@ public class MainActivity extends ActionBarActivity {
             Intent LaunchIntent = getPackageManager().getLaunchIntentForPackage("com.alephzain.framaroot");
             startActivity(LaunchIntent);
         }
+        if (reqCode != 1) {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Framaroot not installed")
+                    .setMessage("Do you want to try installing again?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
     }
 
     public void buttonOnClick2(View v2) {
@@ -94,97 +112,79 @@ public class MainActivity extends ActionBarActivity {
         startActivity(browserIntent);
     }
 
+    /**
+     * Copy the asset at the specified path to this app's data directory. If the
+     * asset is a directory, its contents are also copied.
+     *
+     * @param path Path to asset, relative to app's assets directory.
+     */
+    private void copyAsset(String path) {
+        AssetManager manager = getAssets();
+
+        // If we have a directory, we make it and recurse. If a file, we copy its
+        // contents.
+        try {
+            String[] contents = manager.list(path);
+
+            // The documentation suggests that list throws an IOException, but doesn't
+            // say under what conditions. It'd be nice if it did so when the path was
+            // to a file. That doesn't appear to be the case. If the returned array is
+            // null or has 0 length, we assume the path is to a file. This means empty
+            // directories will get turned into files.
+            if (contents == null || contents.length == 0)
+                throw new IOException();
+
+            // Make the directory.
+            File dir = new File(getExternalFilesDir(null), path);
+            dir.mkdirs();
+
+            // Recurse on the contents.
+            for (String entry : contents) {
+                copyAsset(path + "/" + entry);
+            }
+        } catch (IOException e) {
+            copyFileAsset(path);
+        }
+    }
+
+    /**
+     * Copy the asset file specified by path to app's data directory. Assumes
+     * parent directories have already been created.
+     *
+     * @param path Path to asset, relative to app's assets directory.
+     */
+    private void copyFileAsset(String path) {
+        File file = new File(getExternalFilesDir(null), path);
+        try {
+            InputStream in = getAssets().open(path);
+            OutputStream out = new FileOutputStream(file);
+            byte[] buffer = new byte[1024];
+            int read = in.read(buffer);
+            while (read != -1) {
+                out.write(buffer, 0, read);
+                read = in.read(buffer);
+            }
+            out.close();
+            in.close();
+        } catch (IOException e) {
+            Log.e("MainActivity", "exception", e);
+        }
+    }
+
     public void buttonOnClick3(View v3) {
-        /* (TODO: Fix this Up)
-       String s = getFilesDir().getAbsolutePath();
-        String s1 = (new StringBuilder(String.valueOf(s))).append("/busybox").toString();
-        String s3 = (new StringBuilder(String.valueOf(s))).append("/system").toString();
-        String s4 = (new StringBuilder(String.valueOf(s))).append("/bootmenu").toString();
-        StringBuilder stringbuilder = new StringBuilder();
-
-        Log.d("MainActivity", "one");
-        ///Installing Recovery to System
-        stringbuilder.append((new StringBuilder(String.valueOf(s1))).append(" mount -oremount,rw /system ; ").toString());
-        stringbuilder.append((new StringBuilder(String.valueOf(s1))).append(" cp -r ").append(s3).append(" / ; ").toString());
-        stringbuilder.append("cd /system/bin ; ln -s bootmenu logwrapper ; ");
-
-        Log.d("MainActivity", "two");
-        ///Installing recovery to System/bootmenu/binary/bootmenu
-        stringbuilder.append((new StringBuilder(String.valueOf(s1))).append(" mkdir -p /system/bootmenu/binary/bootmenu ; ").toString());
-        stringbuilder.append((new StringBuilder(String.valueOf(s1))).append(" cp ").append(s4).append(" /system/bootmenu/binary/bootmenu ; ").toString());
-
-        Log.d("MainActivity", "three");
-        //System to ro
-        stringbuilder.append((new StringBuilder(String.valueOf(s1))).append(" mount -oremount,ro /system ; ").toString());
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setPositiveButton(android.R.string.ok, null);
-        try
-        {
-            Helper.runSuCommand(MainActivity.this, stringbuilder.toString());
-            return;
-        }
-        catch (Exception exception)
-        {
-            android.app.AlertDialog.Builder builder2 = new android.app.AlertDialog.Builder(MainActivity.this);
-            builder2.setPositiveButton(0x104000a, null);
-            builder2.setTitle("Failure");              public void buttonOnClick5(View v5) {
-        Process p = null;
-        try {
-            p = Runtime.getRuntime().exec("su -c reboot recovery;");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {                         public void buttonOnClick5(View v5) {
-        Process p = null;
-        try {
-            p = Runtime.getRuntime().exec("su -c reboot recovery;");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (p != null) {
-                p.destroy();               public void buttonOnClick5(View v5) {
-        Process p = null;
-        try {
-            p = Runtime.getRuntime().exec("su -c reboot recovery;");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (p != null) {
-                p.destroy();
-                Log.d("MainActivity", "su was not granted");
-            }
-        }
-        }
-
-                Log.d("MainActivity", "su was not granted");
-            }
-        }
-        }
-
-            if (p != null) {
-                p.destroy();
-                Log.d("MainActivity", "su was not granted");
-            }
-        }
-        }
-
-            builder2.setMessage(exception.getMessage());
-            exception.printStackTrace();
-            return;
-        }
-        */
+        copyAsset("files");
     }
 
     public void buttonOnClick4(View v4) {
         Process p = null;
         try {
-            p = Runtime.getRuntime().exec("su -c reboot;");
+            p = Runtime.getRuntime().exec("su -c reboot");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             if (p != null) {
                 p.destroy();
-                Log.d("MainActivity", "su was not granted");
+                Log.d("MainActivity", "su was not granteds");
             }
         }
     }
@@ -199,6 +199,36 @@ public class MainActivity extends ActionBarActivity {
             if (p != null) {
                 p.destroy();
                 Log.d("MainActivity", "su was not granted");
+                new AlertDialog.Builder(this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Rebooting Failed")
+                        .setMessage("Do you want to try again?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+            }
+        }
+    }
+
+    public void buttonOnClick6(View v6) {
+        Process p = null;
+        try {
+            p = Runtime.getRuntime().exec("su");
+            OutputStream os = p.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
+            dos.writeBytes("reboot" + "\n");
+            dos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (p != null) {
+                p.destroy();
             }
         }
     }
